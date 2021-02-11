82103a232d89da0999f9e60ba2d52552335fe3f1
aydinsenih <aydinsenih@gmail.com>
2021-02-11 10:05:59 -0800

    intial

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/*", (_, res) => {
+    res.status(200).json({ data: "The API lives" });
+});
+
+app.listen(port, () => {
+    console.log("listening on port " + port);
+});
+
+// console.log("alive");
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USERNAME);
+// console.log(process.env.PORT);
+// console.log(dotenv);
+// console.log(dotenv.PORT);
+// console.log(process.env.FOOD);
