47ea5e7a24256145aff3e0ba2242b4fb721f982e
aydinsenih <aydinsenih@gmail.com>
2021-02-11 10:50:18 -0800

    added reactapp

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build", "index.html")));
 
 app.use("/api/*", (_, res) => {
     res.status(200).json({ data: "The API lives" });
